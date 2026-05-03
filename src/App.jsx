f3b4159a74e6999820a894557c339c75b8725339
carolina arroyave <cindycarolina.arroyave@gmail.com>
2026-05-03 06:43:01 -0500

    corrige video y tarjeta tiktok

--- src/App.jsx
+++ src/App.jsx
@@ -468,11 +468,11 @@ Quedo atento(a) a la información para reservar cupo.`;
               <p className="mb-4 text-xl font-black text-slate-900">
                 Aquí no solo juegan… también avanzan.
               </p>
-              <div className="rounded-[2rem] overflow-hidden shadow-2xl border-4 border-cyan-300 bg-slate-950 p-3 h-full">
-                <img
-                  src="/terapeutico.jpeg"
-                  alt="Enfoque terapéutico ADNE"
-                  className="w-full h-full object-contain rounded-[1.rem]"
+              <div className="rounded-[2rem] overflow-hidden shadow-xl h-full">
+               <img
+               src="/terapeutico.png"
+                alt="terapeutico"
+                className="w-full h-full object-cover"
                 />
               </div>
             </div>
